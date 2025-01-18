2c17dca0b5ca1601dd020760e60212813cf271b3
RAGAdox <rishitech882@gmail.com>
2025-01-18 09:28:56 +0530

    feat(topNavigation): implement mobile version of topNavigation

--- app/components/TopNavigation/TopNavigation.tsx
+++ app/components/TopNavigation/TopNavigation.tsx
@@ -1,3 +1,5 @@
+import { DynamicIcon } from "lucide-react/dynamic";
+import { useState } from "react";
 import "./TopNavigation.scss";
 
 interface ITopNavigationLink {
@@ -31,26 +33,59 @@ const TopNavigation = ({
   refArray: React.RefObject<HTMLElement>[];
   currentIndex: number;
 }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const navigateToIndex = (index: number) => {
+    refArray[index].current?.scrollIntoView({
+      behavior: "smooth",
+      inline: "start",
+    });
+    window.location.hash = "";
+  };
+
   return (
-    <nav className="nav-container">
-      {topNavigationLinks.map((navItem, index) => (
-        <button
-          key={index}
-          onClick={() => {
-            refArray[index].current?.scrollIntoView({
-              behavior: "smooth",
-              inline: "start",
-            });
-            window.location.hash = "";
-          }}
-          className={`nav-item ${
-            currentIndex === index ? "nav-item-active" : ""
-          }`}
-        >
-          {navItem.display}
-        </button>
-      ))}
-    </nav>
+    <>
+      <nav className="nav-container">
+        <div className="hidden md:flex flex-row border rounded-full border-white/15 backdrop-blur-3xl bg-slate-400/50 gap-x-4">
+          {topNavigationLinks.map((navItem, index) => (
+            <button
+              key={index}
+              onClick={() => {
+                navigateToIndex(index);
+              }}
+              className={`nav-item ${
+                currentIndex === index ? "nav-item-active" : ""
+              }`}
+            >
+              {navItem.display}
+            </button>
+          ))}
+        </div>
+        <div className="relative flex flex-row md:hidden z-50">
+          <button onClick={() => setIsOpen(!isOpen)}>
+            <DynamicIcon name="menu" />
+          </button>
+        </div>
+        {isOpen && (
+          <div className=" backdrop-blur-3xl h-svh fixed inset-0 z-40 flex flex-col justify-center  gap-y-4">
+            {topNavigationLinks.map((link, index) => (
+              <button
+                onClick={() => {
+                  setIsOpen(false);
+                  navigateToIndex(index);
+                }}
+                className={`w-1/2 mx-auto text-lg nav-item ${
+                  index === currentIndex ? "nav-item-active" : ""
+                }`}
+                key={index}
+              >
+                {link.display}
+              </button>
+            ))}
+          </div>
+        )}
+      </nav>
+    </>
   );
 };
 
