bd063dde9b976893880cf1d2d53e49e2252f1f47
RAGAdox <rishitech882@gmail.com>
2025-01-18 08:05:29 +0530

    chore(css): Hero-Section-2 Style Migration

--- app/routes/home/components/HeroSection2/HeroSection2.tsx
+++ app/routes/home/components/HeroSection2/HeroSection2.tsx
@@ -131,19 +131,16 @@ const ExperienceModal = ({
   return (
     <div
       ref={containerRef}
-      className="z-50 fixed inset-0 backdrop-blur-3xl content-center"
+      className="experience-modal"
       onClick={handleClickOutside}
     >
-      <div
-        ref={cardRef}
-        className="relative flex max-h-svh md:w-1/2 lg:w-1/3 mx-auto flex-col gap-y-4 p-2 rounded-lg border overflow-scroll"
-      >
+      <div ref={cardRef} className="experience-modal-card">
         <div>
-          <div className="flex flex-row items-start justify-between gap-x-4">
+          <div className="experience-modal-card-header">
             <p className="text-2xl font-serif">{organization}</p>
             <button
               autoFocus
-              className=" text-2xl self-start"
+              className="text-2xl self-start"
               onClick={closeModal}
             >
               <DynamicIcon name="x" />
@@ -162,9 +159,9 @@ const ExperienceModal = ({
         </div>
         <div>
           <p className="text-2xl font-serif">Technologies Used -</p>
-          <ul className="flex flex-row flex-wrap gap-x-4 gap-y-2">
+          <ul className="experience-modal-card-footer">
             {technologies.map((value, index) => (
-              <li key={index} className="bg-black rounded-lg px-2 text-nowrap">
+              <li key={index} className="experience-modal-card-footer-pill">
                 {value}
               </li>
             ))}
@@ -185,11 +182,8 @@ const ExperienceCard = ({
   setCurrentIndex: (value: number) => void;
 }) => {
   return (
-    <div
-      key={index}
-      className="flex flex-col gap-y-4 border rounded-lg w-full md:w-1/2 lg:w-1/3 backdrop-blur-md"
-    >
-      <div className="flex flex-col px-2">
+    <div key={index} className="experience-card">
+      <div className="experience-card-header">
         <p className="text-lg font-serif">{experienceDetail.organization}</p>
         <p
           className="text-lg font-serif
@@ -199,10 +193,13 @@ const ExperienceCard = ({
         <p className="">{experienceDetail.role}</p>
       </div>
       <button
-        className="border text-left px-2  bg-slate-100 py-2 rounded-lg text-slate-900 hover:bg-slate-100/75"
+        className="experience-card-cta"
         onClick={() => setCurrentIndex(index)}
       >
-        Know More {"->"}
+        Know More
+        <span>
+          <DynamicIcon name="arrow-right" />
+        </span>
       </button>
     </div>
   );
@@ -232,7 +229,7 @@ const HeroSection2 = ({
         transition={{ duration: 0.5 }}
         id="experience"
         ref={containerRef}
-        className="relative h-svh flex flex-col px-2 md:w-full md:items-center justify-center  gap-y-4 md:snap-start"
+        className="hero-section-2"
       >
         {currentIndex !== undefined && (
           <ExperienceModal
