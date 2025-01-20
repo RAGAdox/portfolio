902ebc257f2b68e59e3fc7984d5ddb928d744e77
RAGAdox <rishitech882@gmail.com>
2025-01-20 07:33:00 +0530

    feat(redisign): redesign about skills and experience

--- app/components/Background/Background.tsx
+++ app/components/Background/Background.tsx
@@ -6,22 +6,40 @@ const Background = ({ currentIndex }: { currentIndex: number }) => {
   const [isInitial, setIsInitial] = useState(true);
   const [scope, animate] = useAnimate();
 
-  const animateCircle = async () => {
+  const animateCircleDesktop = async () => {
     await animate(
-      "#circle-1",
+      "#circle-desktop",
       { scale: isInitial ? [0, 100] : [100, 50, 100] },
       { duration: 0.5, ease: "circInOut" }
     );
   };
 
+  const animateCircleMobile = async () => {
+    if (isInitial) {
+      await animate(
+        "#circle-mobile",
+        { scale: [0, 100] },
+        { duration: 0.5, ease: "easeOut" }
+      );
+    }
+  };
+
   useEffect(() => {
-    animateCircle();
+    animateCircleDesktop();
+    animateCircleMobile();
     setIsInitial(false);
   }, [currentIndex]);
 
   return (
     <div ref={scope} className="background flex justify-center items-center">
-      <div id="circle-1" className="circle w-1 h-1 blur-sm"></div>
+      <div
+        id="circle-desktop"
+        className="circle w-1 h-1 blur-sm hidden md:inline"
+      ></div>
+      <div
+        id="circle-mobile"
+        className="circle w-1 h-1 blur-sm inline md:hidden"
+      ></div>
     </div>
   );
 };
--- app/routes/home/components/HeroSection3/HeroSection3.tsx
+++ app/routes/home/components/HeroSection3/HeroSection3.tsx
@@ -1,160 +1,147 @@
-import { motion, useInView } from "motion/react";
-import { useEffect, useRef, useState } from "react";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AnimatePresence, motion, useAnimate, useInView } from "motion/react";
+import { useEffect, useState } from "react";
 
-const projectDetails: ProjectProps[] = [
-  {
-    name: "Anti-Raging Android Application",
-    active: true,
-    description:
-      "Anti-Raging android application was built for reducing and responding to bulling incidents on and off campus.",
-    technologies: [
-      "React-Native",
-      "Expo",
-      "Node.js",
-      "MongoDB Atlas",
-      "Express.js",
-      "MailChimp",
-    ],
-  },
-  {
-    name: "Portfolio Website",
-    active: true,
-    description:
-      "A portfolio website to showcase skill and get to use new technologies like react-router v7",
-    technologies: ["React", "React-Router V7", "Tailwind CSS"],
-  },
-  {
-    name: "Zenith",
-    active: false,
-    description:
-      "Open Source React SSR Framework that is completely customizable",
-    technologies: ["React V19", "Express.js", "Vite"],
-  },
-  {
-    name: "Imaginfolio-Auth",
-    description: "Plug and Play authentication service",
-    active: true,
-    technologies: ["Node.js", "Koa.js", "Zod", "PostgreSQL", "Redis"],
-  },
-];
-
-const ProjectModal = ({
-  projectDetail,
-  setCurrentIndex,
-}: {
-  projectDetail: ProjectProps;
-  setCurrentIndex: (value: number | undefined) => void;
-}) => {
-  const { name, description, technologies } = projectDetail;
-
-  const cardRef = useRef<HTMLDivElement>(null);
-  const containerRef = useRef<HTMLDivElement>(null);
-
-  const closeModal = () => {
-    setCurrentIndex(undefined);
-  };
-
-  const handleClickOutside = (e: any) => {
-    if (
-      containerRef.current &&
-      cardRef.current &&
-      !cardRef.current.contains(e.target)
-    ) {
-      closeModal();
-    }
-  };
-
-  return (
-    <div
-      className="z-50 fixed inset-0 backdrop-blur-3xl content-center"
-      ref={containerRef}
-      onClick={handleClickOutside}
-    >
-      <div
-        ref={cardRef}
-        className="relative flex max-h-svh md:w-1/2 lg:w-1/3 mx-auto flex-col gap-y-4 p-2 rounded-lg border overflow-y-scroll"
-      >
-        <div>
-          <div className="flex flex-row justify-between">
-            <p className="text-2xl font-serif">{name}</p>
-            <button
-              className="text-2xl self-start"
-              autoFocus
-              onClick={closeModal}
-            >
-              {/* <DynamicIcon name="x" /> */}X
-            </button>
-          </div>
-          <p className="text-lg">{description}</p>
-        </div>
-        <div>
-          <p className="text-2xl">Technologies Used -</p>
-          <ul className="flex flex-row flex-wrap gap-x-4 gap-y-2">
-            {technologies.map((value, index) => (
-              <li className="bg-black rounded-lg px-2 text-nowrap" key={index}>
-                {value}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+const skills = {
+  "programming languages": ["Typescript", "Java", "Python", "C/C++", "SQL"],
+  "frontend development": [
+    "React.js",
+    "Next.js",
+    "React-Router",
+    "SCSS",
+    "TailwindCSS",
+    "Bootstrap",
+    "Redux",
+    "Zustand",
+  ],
+  "backend development": [
+    "Node.js",
+    "Express.js",
+    "Koa.js",
+    "RESTful APIs",
+    "Spring Boot",
+    "GraohQL",
+  ],
+  Databases: ["PostgresSQL", "MongoDB", "Redis", "MySQL", "SQLite"],
+  "Cloud and DevOps": [
+    "Microsoft Azure",
+    "Google Cloud",
+    "Docker",
+    "Kubernetes",
+    "Nginx",
+    "HAProxy",
+    "Github Actions",
+    "Travis CI",
+  ],
+  "Tools and Others": ["Git", "Jira", "Agile", "System Design", "Testing"],
 };
 
 const HeroSection3 = ({
   containerRef,
   setInView,
 }: {
-  containerRef: React.RefObject<HTMLElement>;
+  containerRef: React.RefObject<HTMLDivElement>;
   setInView: () => void;
 }) => {
+  const [scope, animate] = useAnimate();
   const isInView = useInView(containerRef, { amount: 0.5, once: false });
   const [currentIndex, setCurrentIndex] = useState<number>();
+
+  const handlePageAnimations = async () => {
+    await animate(
+      scope.current,
+      { opacity: isInView ? [0, 1] : [1, 0] },
+      { duration: 0.5 }
+    );
+  };
 
   useEffect(() => {
     if (isInView) {
       setInView();
     }
+    if (!isInView) {
+      setCurrentIndex(undefined);
+    }
+    handlePageAnimations();
   }, [isInView]);
 
+  const handleSkillContainerClick = async (index: number) => {
+    if (currentIndex === index) {
+      setCurrentIndex(undefined);
+    } else {
+      setCurrentIndex(index);
+    }
+  };
+
   return (
-    <motion.section
-      initial={{ opacity: 0 }}
-      animate={{ opacity: isInView ? 1 : 0 }}
-      transition={{ duration: 0.5 }}
-      ref={containerRef}
-      className="relative h-svh flex flex-col px-2 md:w-full md:items-center justify-center  gap-y-4 md:snap-start"
-    >
-      {currentIndex !== undefined && (
-        <ProjectModal
-          projectDetail={projectDetails[currentIndex]}
-          setCurrentIndex={setCurrentIndex}
-        />
-      )}
-      {projectDetails.map(
-        ({ name, description, technologies, active }, index) => (
-          <div className="flex flex-col gap-y-4 border rounded-lg w-full md:w-1/2 lg:w-1/3 backdrop-blur-md">
-            <div className="flex flex-row items-center justify-between">
-              <p className="text-2xl font-serif px-2">{name}</p>
-              {active !== undefined && (
-                <span
-                  className={`w-1 h-1 ${
-                    active ? "bg-green-500" : "bg-red-500"
-                  } rounded-full mr-4`}
-                ></span>
-              )}
-            </div>
-            <button
-              className="border text-left px-2  bg-slate-100 py-2 rounded-lg text-slate-900 hover:bg-slate-100/75"
-              onClick={() => setCurrentIndex(index)}
-            >
-              Know More {"->"}
-            </button>
+    <div ref={containerRef}>
+      <section
+        ref={scope}
+        className="h-svh md:snap-start flex items-center px-2 w-full md:1/2 lg:w-2/3 mx-auto"
+      >
+        <div id="skills-div" className="w-full">
+          <p className="text-2xl font-mono">Skills</p>
+          <div className="grid gap-2">
+            {Object.entries(skills).map(
+              ([category, categorySkills], categoryIndex) => {
+                return (
+                  <div
+                    id={`skill-container-${categoryIndex}`}
+                    key={categoryIndex}
+                    className=""
+                  >
+                    <button
+                      className="flex flex-row gap-x-2 items-center "
+                      onClick={() => {
+                        handleSkillContainerClick(categoryIndex);
+                      }}
+                    >
+                      <p className="capitalize text-lg font-mono hover:underline">
+                        {category}
+                      </p>
+                      <motion.span
+                        animate={{
+                          transform:
+                            currentIndex === categoryIndex
+                              ? "rotate(90deg)"
+                              : "rotate(0deg)",
+                        }}
+                        transition={{ duration: 0.2 }}
+                        className=" inline-block"
+                      >
+                        <FontAwesomeIcon icon={faArrowRight} size="lg" />
+                      </motion.span>
+                    </button>
+                    <AnimatePresence initial={false}>
+                      <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-none xl:grid-flow-col gap-4">
+                        {currentIndex === categoryIndex &&
+                          categorySkills.map((skill, skillIndex) => (
+                            <motion.button
+                              key={skillIndex}
+                              initial={{ scale: 0, opacity: 0 }}
+                              animate={{ scale: 1, opacity: 1 }}
+                              exit={{ scale: 0, opacity: 0 }}
+                              className={`skill-box border rounded-lg p-4 text-nowrap`}
+                              transition={{
+                                duration: 0.3,
+                                delay: skillIndex / 20,
+                                ease: "easeInOut",
+                              }}
+                            >
+                              {skill}
+                            </motion.button>
+                          ))}
+                      </div>
+                    </AnimatePresence>
+                  </div>
+                );
+              }
+            )}
           </div>
-        )
-      )}
-    </motion.section>
+        </div>
+      </section>
+    </div>
   );
 };
 
